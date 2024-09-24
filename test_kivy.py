6a5fa25a2ffed0eb76d72e0db34b52e9166c454f
Jamal Salih <93874336+BigBrainsVoid1@users.noreply.github.com>
2024-09-24 05:58:22 +0300

    I

---
+++ test_kivy.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class TestApp(App):
+    def build(self):
+        return Label(text='Hello, Kivy!')
+
+if __name__ == '__main__':
+    TestApp().run()
